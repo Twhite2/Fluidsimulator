41fc6a407cc4043c203f9aed198f918980c54994
Twhite2 <emmanuelmieye@gmail.com>
2024-11-29 18:51:58 +0100

    fluid sim code basics

---
+++ InitGui.py
@@ -0,0 +1,154 @@
+# ***************************************************************************
+# *   (c) bernd hahnebach (bernd@bimstatik.org) 2014                        *
+# *   (c) qingfeng xia @ iesensor.com 2016                                  *
+# *   Copyright (c) 2017 Andrew Gill (CSIR) <agill@csir.co.za>              *
+# *   Copyright (c) 2019-2021 Oliver Oxtoby <oliveroxtoby@gmail.com>        *
+# *   Copyright (c) 2022 Jonathan Bergh <bergh.jonathan@gmail.com>          *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License as        *
+# *   published by the Free Software Foundation, either version 3 of the    *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with this program.  If not,                             *
+# *   see <https://www.gnu.org/licenses/>.                                  *
+# *                                                                         *
+# **************************************************************************/
+
+import sys
+
+
+class CfdOFWorkbench(Workbench):
+    """CfdOF workbench object"""
+
+    def __init__(self):
+        import os
+        from CfdOF import CfdTools
+        from PySide import QtCore
+        from CfdOF.CfdPreferencePage import CfdPreferencePage
+
+        translate = FreeCAD.Qt.translate
+        translations_path = os.path.join(CfdTools.getModulePath(), "Translations")
+        FreeCADGui.addLanguagePath(translations_path)
+        FreeCADGui.updateLocale()
+
+        icon_path = os.path.join(CfdTools.getModulePath(), "Gui", "Icons", "cfd.svg")
+        self.__class__.Icon = icon_path
+        self.__class__.MenuText = translate("Workbench", "CfdOF")
+        self.__class__.ToolTip = translate("Workbench", "CfdOF workbench")
+
+        icons_path = os.path.join(CfdTools.getModulePath(), "Gui", "Icons")
+        QtCore.QDir.addSearchPath("icons", icons_path)
+        FreeCADGui.addPreferencePage(CfdPreferencePage, "CfdOF")
+
+    def Initialize(self):
+        from PySide.QtCore import QT_TRANSLATE_NOOP
+
+        from CfdOF.CfdAnalysis import CommandCfdAnalysis
+        from CfdOF.Mesh.CfdMesh import CommandCfdMeshFromShape
+        from CfdOF.Mesh.CfdMeshRefinement import CommandMeshRegion
+        from CfdOF.Solve.CfdPhysicsSelection import CommandCfdPhysicsSelection
+        from CfdOF.Solve.CfdFluidMaterial import CommandCfdFluidMaterial
+        from CfdOF.Solve.CfdSolverFoam import CommandCfdSolverFoam
+        from CfdOF.Solve.CfdInitialiseFlowField import CommandCfdInitialiseInternalFlowField
+        from CfdOF.Solve.CfdFluidBoundary import CommandCfdFluidBoundary
+        from CfdOF.Solve.CfdZone import CommandCfdPorousZone
+        from CfdOF.Solve.CfdZone import CommandCfdInitialisationZone
+        from CfdOF.Mesh.CfdDynamicMeshRefinement import CommandGroupDynamicMeshRefinement, \
+            CommandDynamicMeshInterfaceRefinement, CommandDynamicMeshShockRefinement
+        from CfdOF.PostProcess.CfdReportingFunction import CommandCfdReportingFunction
+        from CfdOF.Solve.CfdScalarTransportFunction import CommandCfdScalarTransportFunction
+        from CfdOF.CfdOpenPreferencesPage import CommandCfdOpenPreferencesPage
+        from CfdOF.CfdReloadWorkbench import CommandCfdReloadWorkbench
+
+        FreeCADGui.addCommand('CfdOF_Analysis', CommandCfdAnalysis())
+        FreeCADGui.addCommand('CfdOF_MeshFromShape', CommandCfdMeshFromShape())
+        FreeCADGui.addCommand('CfdOF_MeshRegion', CommandMeshRegion())
+        FreeCADGui.addCommand('CfdOF_DynamicMeshInterfaceRefinement', CommandDynamicMeshInterfaceRefinement())
+        FreeCADGui.addCommand('CfdOF_DynamicMeshShockRefinement', CommandDynamicMeshShockRefinement())
+        FreeCADGui.addCommand('CfdOF_GroupDynamicMeshRefinement', CommandGroupDynamicMeshRefinement())
+        FreeCADGui.addCommand('CfdOF_PhysicsModel', CommandCfdPhysicsSelection())
+        FreeCADGui.addCommand('CfdOF_FluidMaterial', CommandCfdFluidMaterial())
+        FreeCADGui.addCommand('CfdOF_FluidBoundary', CommandCfdFluidBoundary())
+        FreeCADGui.addCommand('CfdOF_InitialiseInternal', CommandCfdInitialiseInternalFlowField())
+        FreeCADGui.addCommand('CfdOF_PorousZone', CommandCfdPorousZone())
+        FreeCADGui.addCommand('CfdOF_InitialisationZone', CommandCfdInitialisationZone())
+        FreeCADGui.addCommand('CfdOF_SolverControl', CommandCfdSolverFoam())
+        FreeCADGui.addCommand('CfdOF_ReportingFunctions', CommandCfdReportingFunction())
+        FreeCADGui.addCommand('CfdOF_ScalarTransportFunctions', CommandCfdScalarTransportFunction())
+        FreeCADGui.addCommand('CfdOF_OpenPreferences', CommandCfdOpenPreferencesPage())
+        FreeCADGui.addCommand('CfdOF_ReloadWorkbench', CommandCfdReloadWorkbench())
+
+        cmdlst = ['CfdOF_Analysis',
+                  'CfdOF_MeshFromShape', 'CfdOF_MeshRegion',
+                  (QT_TRANSLATE_NOOP("Workbench", "Dynamic mesh refinement"),
+                   ['CfdOF_DynamicMeshInterfaceRefinement','CfdOF_DynamicMeshShockRefinement',]),
+                  ('CfdOF_GroupDynamicMeshRefinement',),
+                  'CfdOF_PhysicsModel', 'CfdOF_FluidMaterial',
+                  'CfdOF_FluidBoundary', 'CfdOF_InitialiseInternal',
+                  'CfdOF_InitialisationZone', 'CfdOF_PorousZone',
+                  'CfdOF_ReportingFunctions', 'CfdOF_ScalarTransportFunctions',
+                  'CfdOF_SolverControl']
+
+        for cmd in cmdlst:
+            if isinstance(cmd, tuple):
+                if len(cmd) == 1:
+                    self.appendToolbar(QT_TRANSLATE_NOOP("Workbench", "CfdOF"), [cmd[0]])
+                else:
+                    self.appendMenu([QT_TRANSLATE_NOOP("Workbench", "&CfdOF"), cmd[0]], cmd[1])
+            else:
+                self.appendMenu(QT_TRANSLATE_NOOP("Workbench", "&CfdOF"), [cmd])
+                self.appendToolbar(QT_TRANSLATE_NOOP("Workbench", "CfdOF"), [cmd])
+
+        self.appendMenu(QT_TRANSLATE_NOOP("Workbench", "&CfdOF"),
+                        ["CfdOF_OpenPreferences", "CfdOF_ReloadWorkbench"])
+
+        from CfdOF import CfdTools
+        prefs = CfdTools.getPreferencesLocation()
+        CfdTools.DockerContainer.usedocker = FreeCAD.ParamGet(prefs).GetBool("UseDocker", 0)    
+
+    def GetClassName(self):
+        return "Gui::PythonWorkbench"
+
+    def __del__(sef):
+        from CfdOF import CfdTools
+        if CfdTools.DockerContainer.container_id != None:
+            CfdTools.docker_container.stop_container()
+
+import CfdOF
+FreeCADGui.addWorkbench(CfdOFWorkbench())
+FreeCAD.__unit_test__ += ["TestCfdOF"]
+
+# Create backward compatible aliases for loading from file when modules have moved
+from CfdOF import CfdAnalysis
+sys.modules['CfdAnalysis'] = CfdAnalysis
+from CfdOF.Solve import CfdPhysicsSelection
+sys.modules['CfdPhysicsSelection'] = CfdPhysicsSelection
+from CfdOF.Solve import CfdFluidMaterial
+sys.modules['CfdFluidMaterial'] = CfdFluidMaterial
+from CfdOF.Solve import CfdInitialiseFlowField
+sys.modules['CfdInitialiseFlowField'] = CfdInitialiseFlowField
+from CfdOF.Mesh import CfdMesh
+sys.modules['CfdMesh'] = CfdMesh
+from CfdOF.Mesh import CfdMeshRefinement
+sys.modules['CfdMeshRefinement'] = CfdMeshRefinement
+from CfdOF.Solve import CfdFluidBoundary
+sys.modules['CfdFluidBoundary'] = CfdFluidBoundary
+from CfdOF.Solve import CfdZone
+sys.modules['CfdZone'] = CfdZone
+from CfdOF.Solve import CfdSolverFoam
+sys.modules['CfdSolverFoam'] = CfdSolverFoam
+from CfdOF.PostProcess import CfdReportingFunction
+sys.modules['core.functionobjects.reporting.CfdReportingFunctions'] = CfdReportingFunction
+sys.modules['CfdOF.PostProcess.CfdReportingFunctions'] = CfdReportingFunction
+from CfdOF.Solve import CfdScalarTransportFunction
+sys.modules['core.functionobjects.scalartransport.CfdScalarTransportFunction'] = CfdScalarTransportFunction
+from CfdOF.Mesh import CfdDynamicMeshRefinement
+sys.modules['core.mesh.dynamic.CfdDynamicMeshRefinement'] = CfdDynamicMeshRefinement
+sys.modules['core'] = CfdOF
